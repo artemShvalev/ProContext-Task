3bcbbb71a626290e1159e0423875fc6cdbf52c91
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-06-01 14:13:58 +0500

    lists build

--- src/components/ListLeft.vue
+++ src/components/ListLeft.vue
@@ -1,47 +1,50 @@
 <template>
 <div>
  <ul>
-    <span></span>
-    <input type="checkbox"> List 1
-    <li>Item 1</li>
-    <li>item 2</li>
-    <li>item 3</li>
-    <li>item 4</li>
+    <input type="radio" id="list">
+    <label for="list">List 1 <span></span></label>
+
+    <input type="checkbox" id="item-1"> <li>Item 1</li>
+    <input type="checkbox"> <li>Item 2</li>
+    <input type="checkbox"> <li>Item 3</li>
+    <input type="checkbox"> <li>Item 4</li>
   </ul>
   <ul>
-    <span></span>
-  <input type="checkbox">  List 2
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
+    <input type="radio" id="list-2">
+    <label for="list-2">List 2 <span></span></label>
+
+    <input type="checkbox" id="item-2"> <li>Item 1</li>
+    <input type="checkbox"> <li>Item 2</li>
+    <input type="checkbox"> <li>Item 3</li>
+    <input type="checkbox"> <li>Item 4</li>
   </ul>
   <ul>
-    <span></span>
-    <input type="checkbox">  List 3
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
+    <input type="radio" id="list-3">
+    <label for="list-3">List 3 <span></span></label>
+
+    <input type="checkbox" id="item-3"> <li>Item 1</li>
+    <input type="checkbox"> <li>Item 2</li>
+    <input type="checkbox"> <li>Item 3</li>
+    <input type="checkbox"> <li>Item 4</li>
   </ul>
   <ul>
-    <span></span>
-    <input type="checkbox">  List 4
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-    <li>Item 4</li>
+    <input type="radio" id="list-4">
+    <label for="list-4">List 4 <span></span></label>
+
+    <input type="checkbox" id="item-4"> <li>Item 1</li>
+    <input type="checkbox"> <li>Item 2</li>
+    <input type="checkbox"> <li>Item 3</li>
+    <input type="checkbox"> <li>Item 4</li>
   </ul>
 </div>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style lang="scss">
-  @import '@/assets/styles/main.scss';
+@import "@/assets/styles/main.scss";
 </style>
 
 // Кинуть Пропсы в райт коипонент
