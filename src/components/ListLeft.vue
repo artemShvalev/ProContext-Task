02f3df17be42ac8b4b85d88dcc2d69a6ce194fea
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-06-01 12:25:40 +0500

    conf styles

--- src/components/ListLeft.vue
+++ src/components/ListLeft.vue
@@ -1,6 +1,7 @@
 <template>
 <div>
-  <ul>
+ <ul>
+    <span></span>
     <input type="checkbox"> List 1
     <li>Item 1</li>
     <li>item 2</li>
@@ -8,6 +9,7 @@
     <li>item 4</li>
   </ul>
   <ul>
+    <span></span>
   <input type="checkbox">  List 2
     <li>Item 1</li>
     <li>Item 2</li>
@@ -15,6 +17,7 @@
     <li>Item 4</li>
   </ul>
   <ul>
+    <span></span>
     <input type="checkbox">  List 3
     <li>Item 1</li>
     <li>Item 2</li>
@@ -22,17 +25,12 @@
     <li>Item 4</li>
   </ul>
   <ul>
+    <span></span>
     <input type="checkbox">  List 4
     <li>Item 1</li>
     <li>Item 2</li>
     <li>Item 3</li>
     <li>Item 4</li>
-  </ul>
-  <ul>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
   </ul>
 </div>
 </template>
