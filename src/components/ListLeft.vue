04e610bc88d46ce9e53287da7d6b9b7125b5ae79
artemShvalev <60879559+artemShvalev@users.noreply.github.com>
2021-06-01 17:48:52 +0500

    add toogle

--- src/components/ListLeft.vue
+++ src/components/ListLeft.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
  <ul>
-    <input type="radio" id="list">
+    <input type="radio" id="list" v-model="list"   @click="list1 = !list1, list = !list">
     <label for="list">List 1 <span></span></label>
 
-    <input type="checkbox" id="item-1"> <li>Item 1</li>
-    <input type="checkbox"> <li>Item 2</li>
-    <input type="checkbox"> <li>Item 3</li>
-    <input type="checkbox"> <li>Item 4</li>
+    <input type="checkbox" id="item-1" v-model="list1"> <li>Item 1</li>
+    <input type="checkbox" v-model="list1"> <li>Item 2</li>
+    <input type="checkbox" v-model="list1"> <li>Item 3</li>
+    <input type="checkbox" v-model="list1"> <li>Item 4</li>
   </ul>
   <ul>
-    <input type="radio" id="list-2">
+    <input type="radio" id="list-2" v-model="list2"  @click.stop="list2 = !list2">
     <label for="list-2">List 2 <span></span></label>
 
     <input type="checkbox" id="item-2"> <li>Item 1</li>
@@ -40,7 +40,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      list: false,
+      list1: false,
+      list2: false,
+      list3: false,
+      list4: false,
+    };
+  },
+  methods: {
+  },
+};
 </script>
 
 <style lang="scss">
